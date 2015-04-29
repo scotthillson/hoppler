a9a0558ea6a91013ab161c577bada3107c5926e8
scotthillson <scott.hillson@gmail.com>
2015-04-29 16:23:23 -0700

    functions are good

--- app/assets/javascripts/document.js
+++ app/assets/javascripts/document.js
@@ -1,3 +1,13 @@
+var get_param = function(variable){
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if(pair[0] == variable){
+      return pair[1];
+    }
+  }
+}
 var ready = function(){
   initialize();
   $('#avalanche').on('click','avalanche');
