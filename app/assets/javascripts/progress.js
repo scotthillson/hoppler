834b0d80ff11119c545e1ac21cab3e1d1f933e67
scotthillson <scott.hillson@gmail.com>
2015-05-11 14:19:37 -0700

    i do not want to fuss with the loading progress right now

--- app/assets/javascripts/progress.js
+++ app/assets/javascripts/progress.js
@@ -1,24 +1,13 @@
 var draw_progress = function(){
   var div = document.createElement('div');
-  div.style.backgroundColor = 'blue';
+  div.style.backgroundColor = 'gray';
   div.style.position = 'fixed';
   div.style.height = '8px';
+  div.style.width = '100%';
   div.style.left = 0;
   div.style.top = 0;
   document.body.appendChild(div);
   progress_div = div;
-  loading_progress(0);
-}
-var loading_progress = function(progress){
-  progress+=1;
-  if(progress>100){
-    progress=0;
-  }
-  progress_div.style.width = progress+'%';
-  if(cycles<1){
-    console.log('hey');
-    setTimeout(loading_progress(progress),10);
-  }
 }
 var draw_times = function(){
   var div = document.createElement('div');
