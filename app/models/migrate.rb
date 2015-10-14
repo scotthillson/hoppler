d18f3244099b8556a944f803925ed995e59646f5
Scott Hillson <scott.hillson@gmail.com>
2015-10-14 13:31:02 -0700

    dont need to save twice

--- app/models/migrate.rb
+++ app/models/migrate.rb
@@ -5,22 +5,18 @@ class Migrate < ActiveRecord::Base
     self.table_name = :images
     Image.all.each do |i|
       m = Migrate.create(i.attributes)
-      m.save
     end
     self.table_name = :towers
     Tower.all.each do |t|
       m = Migrate.create(t.attributes)
-      m.save
     end
     self.table_name = :locations
     Location.all.each do |l|
       m = Migrate.create(l.attributes)
-      m.save
     end
     self.table_name = :location_lookups
     LocationLookup.all.each do |l|
       m = Migration.create(l.attributes)
-      m.save
     end
   end
   
