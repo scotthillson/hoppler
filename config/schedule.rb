a97b99ade8df32b169eab4e3fe65dcdb9bf99033
scotthillson <scott.hillson@gmail.com>
2015-12-18 11:22:55 -0800

    what the hell is going on in the scheudle

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,17 +1,8 @@
-# Use this file to easily define all of your cron jobs.
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
+# Use this file to easily define all of your cron jobs
+
 # Learn more: http://github.com/javan/whenever
 
 set :environment, 'development'
-
-set :output, '/home/rails/hoppler/log/whenever.log'
 
 every 10.minutes do
   runner "Tower.populate"
