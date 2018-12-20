ea5489611b7e1d48503e0e647d2057847f13cce7
Scott Hillson <scott.hillson@gmail.com>
2018-12-20 09:49:54 -0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,17 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20151014004603) do
 
-  create_table "images", force: true do |t|
+  create_table "images", force: :cascade do |t|
     t.integer  "tower_id"
     t.datetime "time"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
+    t.string   "image",      limit: 255
+    t.index ["time"], name: "index_images_on_time"
   end
 
-  add_index "images", ["time"], name: "index_images_on_time"
-
-  create_table "location_lookups", force: true do |t|
+  create_table "location_lookups", force: :cascade do |t|
     t.integer  "rank"
     t.integer  "tower_id"
     t.integer  "location_id"
@@ -31,38 +29,38 @@ ActiveRecord::Schema.define(version: 20151014004603) do
     t.datetime "updated_at"
   end
 
-  create_table "locations", force: true do |t|
-    t.string   "state"
-    t.string   "city"
+  create_table "locations", force: :cascade do |t|
+    t.string   "state",      limit: 255
+    t.string   "city",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.string   "lat"
-    t.string   "lng"
+    t.string   "name",       limit: 255
+    t.string   "lat",        limit: 255
+    t.string   "lng",        limit: 255
   end
 
-  create_table "towers", force: true do |t|
-    t.string   "rid"
-    t.string   "city"
-    t.string   "state"
-    t.string   "sw_lat"
-    t.string   "sw_lng"
-    t.string   "ne_lat"
-    t.string   "ne_lng"
-    t.string   "center_lat"
-    t.string   "center_lng"
+  create_table "towers", force: :cascade do |t|
+    t.string   "rid",         limit: 255
+    t.string   "city",        limit: 255
+    t.string   "state",       limit: 255
+    t.string   "sw_lat",      limit: 255
+    t.string   "sw_lng",      limit: 255
+    t.string   "ne_lat",      limit: 255
+    t.string   "ne_lng",      limit: 255
+    t.string   "center_lat",  limit: 255
+    t.string   "center_lng",  limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "est_sw_lat"
-    t.string   "est_sw_lng"
-    t.string   "est_ne_lat"
-    t.string   "est_ne_lng"
+    t.string   "est_sw_lat",  limit: 255
+    t.string   "est_sw_lng",  limit: 255
+    t.string   "est_ne_lat",  limit: 255
+    t.string   "est_ne_lng",  limit: 255
     t.integer  "location_id"
   end
 
-  create_table "uploads", force: true do |t|
-    t.string   "url"
-    t.string   "note"
+  create_table "uploads", force: :cascade do |t|
+    t.string   "url",        limit: 255
+    t.string   "note",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
